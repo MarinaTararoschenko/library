c948b0562bf7db57f2ae4db46aa7924a0b26cedb
Marina Tararoshenko <tararoshenkom@gmail.com>
2025-05-30 11:57:58 +0300

    fix: minor fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, Renderer2, ViewEncapsulation } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -10,17 +11,19 @@ export class AppComponent {
     isOpenedSidenav = false;
 
     constructor(
+        @Inject(DOCUMENT) private doc:Document,
         private renderer: Renderer2
     ) {
     }
 
     getScrollEvent(ev: any): void {
+        const body = this.doc.body;
         const startScroll = window.innerWidth > 670 ? 64 : 48;
 
         if (ev.srcElement.scrollTop >= startScroll) {
-            this.renderer.addClass(document.body, 'fixed-header');
+            this.renderer.addClass(body, 'fixed-header');
         } else {
-            this.renderer.removeClass(document.body, 'fixed-header');
+            this.renderer.removeClass(body, 'fixed-header');
         }
     }
 
