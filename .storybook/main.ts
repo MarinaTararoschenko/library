233126bbb86c21c9a58891895d06360c4eca623f
Marina Tararoshenko <tararoshenkom@gmail.com>
2025-08-06 17:48:56 +0300

    feat: setting up display of material icons in svg

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,34 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/angular",
-    "options": {}
-  }
+    stories: [
+        "../src/**/*.mdx",
+        "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    ],
+    addons: [
+        "@storybook/addon-essentials",
+        "@storybook/addon-onboarding",
+        "@storybook/addon-interactions"
+    ],
+    framework: {
+        name: "@storybook/angular",
+        options: {}
+    },
+    staticDirs: [
+        {
+            from: '../src/assets', // Path to the source folder with assets in the Angular project
+            to: '/assets' // Virtual path to access Storybook (localhost:6006/assets/)
+        }
+    ],
+    webpackFinal: async (config) => { // for correct operation of material icons, including svg
+        config.module?.rules?.push({
+            test: /\.svg$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'assets/images/icons/[name][ext]'
+            }
+        });
+        return config;
+    }
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,17 +1,50 @@
-import type { Preview } from '@storybook/angular'
+import { applicationConfig, type Preview } from '@storybook/angular'
 import { setCompodocJson } from "@storybook/addon-docs/angular";
+import { ErrorHandler, importProvidersFrom } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
+    decorators: [
+        applicationConfig({
+            providers: [
+                importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
+                {
+                    provide: MatIconRegistry,
+                    useFactory: (http: HttpClient, sanitizer: DomSanitizer) => {
+                        const registry = new MatIconRegistry(
+                            http,
+                            sanitizer,
+                            document,
+                            new class implements ErrorHandler {
+                                handleError(error: any): void {
+                                    console.error('Icon error:', error);
+                                }
+                            }
+                        );
+                        registry.addSvgIconSet(
+                            sanitizer.bypassSecurityTrustResourceUrl('/assets/images/icons/set.svg')
+                        );
+                        return registry;
+                    },
+                    deps: [HttpClient, DomSanitizer]
+                }
+            ],
+        }),
+    ],
 };
 
 export default preview;
